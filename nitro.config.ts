e05d94044767c2e139aa6a0553e7a5a2aed94bab
Milu459 <yesintabella26@gmail.com>
2024-12-28 05:41:57 +0800

    Initial commit

---
+++ nitro.config.ts
@@ -0,0 +1,15 @@
+export default defineNitroConfig({
+  typescript: {
+    strict: true,
+    tsConfig: {
+      compilerOptions: {
+        module: 'preserve',
+        noEmit: true,
+        moduleDetection: 'force',
+        isolatedModules: true,
+        skipLibCheck: true,
+      },
+    },
+  },
+  compatibilityDate: '2024-11-22',
+})
---
+++ routes/set-webhook.ts
@@ -0,0 +1,15 @@
+import process from 'node:process'
+import { bot } from '../src'
+
+const TOKEN = process.env.TOKEN!
+
+export default eventHandler(async (evt) => {
+  const host = getRequestHeader(evt, 'x-forwarded-host') || getRequestHost(evt)
+  const webhookUrl = `https://${host}/telegram-hook?token=${TOKEN}`
+  const isSet = await bot.telegram.setWebhook(webhookUrl)
+  const info = await bot.telegram.getWebhookInfo()
+  return `Set webhook to ${webhookUrl.replaceAll(
+    TOKEN,
+    '*',
+  )}: ${isSet}<br/>${JSON.stringify(info).replaceAll(TOKEN, '*')}`
+})
